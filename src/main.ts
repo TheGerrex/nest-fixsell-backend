67236d7e700ca7a34525616c0ba01ad8d72f6013
Jonas Clausen <jonastclausen@gmail.com>
2023-08-28 17:05:57 -0600

    cors not working for some reason

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,20 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { appendFile } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
 
-  app.useGlobalPipes( 
+  app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
-    })
+    }),
   );
 
   await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
