0a98ffec823e29ac99d12325634af8a7ac205331
Jonas Clausen <jonastclausen@gmail.com>
2023-12-14 16:53:36 -0600

    postgress connections

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { PrintersModule } from './printers/printers.module';
 import { AuthModule } from './auth/auth.module';
 import { EmailModule } from './email/email.module';
-
 
 @Module({
   imports: [
@@ -14,10 +13,34 @@ import { EmailModule } from './email/email.module';
     MongooseModule.forRoot(process.env.MONGO_URI, {
       dbName: process.env.MONGO_DB_NAME,
     }),
+
+    // postgresql
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: 'postgres',
+        host: process.env.POSTGRESS_HOST,
+        port: 5432,
+        password: process.env.POSTGRES_PASSWORD,
+        username: 'postgres',
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        database: 'fixsell_erp',
+
+        synchronize: true,
+        logging: true,
+        ssl: true,
+        extra: {
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+      }),
+    }),
+
     EmailModule,
     PrintersModule,
     AuthModule,
   ],
 })
-export class AppModule {
-}
+export class AppModule {}
