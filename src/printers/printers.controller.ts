7c18b236bef671147e280687315a0d15bad17d55
Jonas Clausen <jonastclausen@gmail.com>
2024-01-03 20:32:29 -0600

    more logging

--- src/printers/printers.controller.ts
+++ src/printers/printers.controller.ts
@@ -20,6 +20,7 @@ export class PrintersController {
 
   @Post()
   create(@Body() createPrinterDto: CreatePrinterDto) {
+    console.log('POST /printers - calling create method');
     console.log(createPrinterDto);
     console.log('creating printer...');
     return this.printersService.create(createPrinterDto);
@@ -32,26 +33,31 @@ export class PrintersController {
 
   @Delete()
   removeAll() {
+    console.log('DELETE /printers - calling removeAll method');
     return this.printersService.removeAll();
   }
 
   @Get()
   findAll(@Query() paginationDto: PaginationDto) {
+    console.log('GET /printers - calling findAll method');
     return this.printersService.findAll(paginationDto);
   }
 
   @Get(':term')
   findOne(@Param('term') term: string): Promise<Printer> {
+    console.log('GET /printers/:term - calling findOne method');
     return this.printersService.findOne(term);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updatePrinterDto: UpdatePrinterDto) {
+    console.log('PATCH /printers/:id - calling update method');
     return this.printersService.update(id, updatePrinterDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
+    console.log('DELETE /printers/:id - calling remove method');
     return this.printersService.remove(id);
   }
 }
