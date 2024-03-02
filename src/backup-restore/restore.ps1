fa0aae13d15b6ed4233d2dbb68bdfedc945f6438
Jonas Clausen <jonastclausen@gmail.com>
2024-03-01 18:20:19 -0600

    fix restore
    test

--- src/backup-restore/restore.ps1
+++ src/backup-restore/restore.ps1
@@ -1,12 +1,23 @@
-# Ask for the name or ID of the Docker container and the path to the backup file
+# Ask for the path to the .env file, the name or ID of the Docker container, and the path to the backup file
+$envFilePath = Read-Host -Prompt 'Enter the path to the development.env file'
 $dockerContainer = Read-Host -Prompt 'Enter the name or ID of the Docker container'
 $backupFilePath = Read-Host -Prompt 'Enter the path to the backup file'
+
+# Load environment variables from .env file
+$env = ConvertFrom-StringData (Get-Content $envFilePath -raw)
+$dbName = $env.POSTGRES_DB_NAME
+
+# Print the database name to the console
+Write-Output "Database name: $dbName"
+
+# Drop the existing database and create a new one
+docker exec -t $dockerContainer bash -c "dropdb -U postgres $dbName --if-exists; createdb -U postgres $dbName;"
 
 # Copy the backup file from the host to the Docker container
 docker cp "$backupFilePath" "$($dockerContainer):/var/lib/postgresql/data/backup.sql"
 
 # Restore the database from the backup file
-docker exec -t $dockerContainer bash -c "psql -U postgres < /var/lib/postgresql/data/backup.sql"
+docker exec -t $dockerContainer bash -c "psql -U postgres -d $dbName < /var/lib/postgresql/data/backup.sql"
 
 # Remove the backup file from the Docker container
 docker exec -t $dockerContainer bash -c "rm /var/lib/postgresql/data/backup.sql"
--- src/backup-restore/restore.sh
+++ src/backup-restore/restore.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-# Ask for the name or ID of the Docker container and the path to the backup file
+# Ask for the path to the .env file, the name or ID of the Docker container, and the path to the backup file
+read -p 'Enter the path to the development.env file: ' envFilePath
 read -p 'Enter the name or ID of the Docker container: ' dockerContainer
 read -p 'Enter the path to the backup file: ' backupFilePath
+
+# Load environment variables from .env file
+source $envFilePath
+dbName=$POSTGRES_DB_NAME
+
+# Print the database name to the console
+echo "Database name: $dbName"
+
+# Drop the existing database and create a new one
+docker exec -t $dockerContainer bash -c "dropdb -U postgres $dbName --if-exists; createdb -U postgres $dbName;"
 
 # Copy the backup file from the host to the Docker container
 docker cp "$backupFilePath" "$dockerContainer:/var/lib/postgresql/data/backup.sql"
 
 # Restore the database from the backup file
-docker exec -t $dockerContainer bash -c "psql -U postgres < /var/lib/postgresql/data/backup.sql"
+docker exec -t $dockerContainer bash -c "psql -U postgres -d $dbName < /var/lib/postgresql/data/backup.sql"
 
 # Remove the backup file from the Docker container
 docker exec -t $dockerContainer bash -c "rm /var/lib/postgresql/data/backup.sql"
