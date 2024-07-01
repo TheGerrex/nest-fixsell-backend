aa4a4366faaeee996fe5a22307d76f3872ee23b8
Jonas Clausen <jonastclausen@gmail.com>
2024-07-01 17:40:22 -0600

    Revert "chat fail"
    This reverts commit c75b2e0131fc6a81d1fbdf81800a518c08f338a8.

--- src/chatbot/chatbot.module.ts
+++ src/chatbot/chatbot.module.ts
@@ -3,7 +3,6 @@ import { ChatbotService } from './chatbot.service';
 import { ChatbotGateway } from './chatbot.gateway';
 import { AuthModule } from 'src/auth/auth.module';
 import { LeadsModule } from 'src/sales/leads/leads.module';
-// import user
 
 @Module({
   providers: [ChatbotGateway, ChatbotService],
