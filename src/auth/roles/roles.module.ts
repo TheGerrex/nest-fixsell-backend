460d1d8688bdffe124bf66fe0252a0c1155d33f9
TheGerrex <gerrex95@gmail.com>
2024-07-19 16:35:52 -0600

    fix in roles remove, update and create

--- src/auth/roles/roles.module.ts
+++ src/auth/roles/roles.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Role } from './entities/role.entity';
 import { RolesService } from './roles.service';
 import { RolesController } from './roles.controller';
+import { User } from '../entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Role])],
+  imports: [TypeOrmModule.forFeature([Role, User]),],
   controllers: [RolesController],
   providers: [RolesService],
 })
--- src/auth/roles/roles.service.ts
+++ src/auth/roles/roles.service.ts
@@ -1,22 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
 import { Role } from './entities/role.entity';
+import { User } from '../entities/user.entity';
 
 @Injectable()
 export class RolesService {
   constructor(
     @InjectRepository(Role)
     private rolesRepository: Repository<Role>,
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
   ) {}
 
   async create(createRoleDto: CreateRoleDto): Promise<Role> {
     const role = this.rolesRepository.create(createRoleDto);
-    await this.rolesRepository.save(role);
+
+    try {
+      await this.rolesRepository.save(role);
+    } catch (error) {
+      if (error.code === '23505') { // 23505 is the code for unique_violation in PostgreSQL
+        throw new ConflictException('El nombre del rol ya existe.');
+      } else {
+        throw error;
+      }
+    }
+
     return role;
   }
+
 
   findAll() {
     return this.rolesRepository.find();
@@ -31,7 +45,15 @@ export class RolesService {
   }
 
   async update(id: string, updateRoleDto: UpdateRoleDto): Promise<Role> {
-    await this.rolesRepository.update(id, updateRoleDto);
+    try {
+      await this.rolesRepository.update(id, updateRoleDto);
+    } catch (error) {
+      if (error.code === '23505') { // 23505 is the code for unique_violation in PostgreSQL
+        throw new ConflictException('El nombre del rol ya existe.');
+      } else {
+        throw error;
+      }
+    }
     return this.rolesRepository.findOne({
       where: {
         id: id,
@@ -39,7 +61,24 @@ export class RolesService {
     });
   }
 
-  remove(id: string) {
+  async remove(id: string) {
+    // Find users with this role
+    const users = await this.usersRepository.createQueryBuilder('user')
+      .innerJoinAndSelect('user.roles', 'role', 'role.id = :id', { id })
+      .getMany();
+  
+    // Remove the role from their roles array
+    for (const user of users) {
+      const roleToRemove = user.roles.find(role => role.id === id);
+      if (roleToRemove) {
+        await this.usersRepository.createQueryBuilder()
+          .relation(User, 'roles')
+          .of(user)
+          .remove(roleToRemove);
+      }
+    }
+  
+    // Delete the role
     return this.rolesRepository.delete(id);
   }
 }
