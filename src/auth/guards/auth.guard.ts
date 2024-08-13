5884cd5534e5b984615a0f767ee505210deee6f5
Jonas Clausen <jonastclausen@gmail.com>
2024-08-13 13:38:14 -0600

    clean up auth.guard

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -5,16 +5,14 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { Observable } from 'rxjs';
 import { JwtPayload } from '../interfaces/jwt-payload';
 import { AuthService } from '../auth.service';
-// import { Role } from '../interfaces/role'; // Import the Role enum
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(
     private jwtService: JwtService,
-    private AuthService: AuthService,
+    private authService: AuthService,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
@@ -30,19 +28,13 @@ export class AuthGuard implements CanActivate {
         secret: process.env.JWT_SEED,
       });
 
-      const user = await this.AuthService.findUserById(payload.id);
+      const user = await this.authService.findUserById(payload.id);
       if (!user) {
         throw new UnauthorizedException('User does not exist');
       }
       if (!user.isActive) {
         throw new UnauthorizedException('User is not active');
       }
-
-      // Check if the user has the 'admin' role
-      // if (!user.roles.some((role) => role.name === 'admin')) {
-      //   throw new UnauthorizedException('User does not have admin privileges');
-      // }
-
       request['user'] = user;
     } catch (error) {
       throw new UnauthorizedException('You are not authorized(Invalid token)');
